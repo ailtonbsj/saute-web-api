c6d66c36119978ad3820e3e3b3d5657726140271
ailtonbsj <ailton.ifce@gmail.com>
2022-08-25 23:45:53 -0300

    Initial

---
+++ src/main/java/ailtonbsj/sauteweb/sauteapi/SauteApiApplication.java
@@ -0,0 +1,13 @@
+package ailtonbsj.sauteweb.sauteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SauteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SauteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ailtonbsj/sauteweb/sauteapi/SauteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ailtonbsj.sauteweb.sauteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SauteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
